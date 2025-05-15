a7a5d3f1bb6b4272ff09365adea0bac40df7ead0
Prem <kumarprem10694@gmail.com>
2025-05-15 20:08:53 +0530

    logger and data updated

---
+++ slides.py
@@ -0,0 +1,5 @@
+slides = [
+    "Meena met Mathur in Lodhi Garden, under the rain.",
+    "They shared chai and laughter in Khan Market.",
+    "Mathur reveals he’s leaving for London in a week."
+]
